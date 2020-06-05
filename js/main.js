a7fbf39d19e0ccc84adbac4161db5e72b28f5bad
mapawel <pawelma@gmail.com>
2020-06-05 02:44:52 +0200

    js for mobiles finished, css for mobiles finished

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ let $content; //api reponce with meals
 let $input;
 let $searchBtn;
 
+let $thumbnailsSection; //thumbnails section
 let $resultsSection; //result section
 let $randomPropSection; //random proposal section
 let $backSection;
@@ -27,7 +28,10 @@ let $chosenMealReceipParent;
 let $searchedMeal;
 let $mealId;
 let $selectedMealsID;
+let $previewMealsID;
 let $receipeElNr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+let $resultsImgBoxes;
+let $nowPreviewed;
 
 
 
@@ -57,6 +61,7 @@ const prepareDomElements = () => {
     $backSection = document.querySelector('.back');
     $backLink = document.querySelector('.back-link');
     $manualSection = document.querySelector('.manual');
+    $thumbnailsSection = document.querySelector('.thubnails');
 }
 
 const prepareEvents = () => {
@@ -101,6 +106,7 @@ const addResult = (meal) => {
     $resultsSection.classList.remove('nodisplay');
     $choiceSection.classList.add('nodisplay');
     $manualSection.classList.add('nodisplay');
+    $backSection.classList.add('nodisplay');
     $input.value = '';
     let newResult = document.createElement('li');
     newResult.classList.add('result');
@@ -119,17 +125,41 @@ const addResult = (meal) => {
     newResult.appendChild(newResultToolsBox);
     let newButtonCheck = document.createElement('button');
     newButtonCheck.classList.add('result-check');
+    newButtonCheck.setAttribute('onclick', 'showImgBox(this)');
     newResultToolsBox.appendChild(newButtonCheck);
     let iconSearch = document.createElement('i')
     iconSearch.classList.add('fas', 'fa-search');
     newButtonCheck.appendChild(iconSearch);
     let newButtonChoose = document.createElement('button');
     newButtonChoose.classList.add('result-choose');
+    newButtonChoose.setAttribute('onclick', 'selectMealfromList(this)');
     newResultToolsBox.appendChild(newButtonChoose);
     let iconChoose = document.createElement('i')
     iconChoose.classList.add('fas', 'fa-check');
-    iconChoose.setAttribute('onclick', 'selectMealfromList(this)')
     newButtonChoose.appendChild(iconChoose);
+
+    let newResultImgBox = document.createElement('div');
+    newResultImgBox.classList.add('result-img-box', 'hidden-img-box');
+    newResultImgBox.setAttribute('id', $mealId);
+    newResult.appendChild(newResultImgBox);
+
+    let newResultImg = document.createElement('img');
+    newResultImg.classList.add('result-img');
+    newResultImg.setAttribute('src', meal.strMealThumb);
+    newResultImgBox.appendChild(newResultImg);
+
+    let newResultImgTxt = document.createElement('p');
+    newResultImgTxt.classList.add('result-thumb-close');
+    newResultImgTxt.innerText = 'click image to close';
+    newResultImgBox.appendChild(newResultImgTxt);
+
+    let newResultImgBtn = document.createElement('button');
+    newResultImgBtn.classList.add('result-thumb-select-btn');
+    newResultImgBtn.setAttribute('id', $mealId);
+    newResultImgBtn.setAttribute('onclick', 'selectMealfromList(this)');
+    newResultImgBtn.innerText = 'check';
+    newResultImgBox.appendChild(newResultImgBtn);
+
     $mealId += 1;
 }
 
@@ -145,8 +175,26 @@ const wrongSearch = () => {
     newResultTxtName.classList.add('result-txt-name');
     newResultTxtName.innerText = "not found - try another dish name...";
     newResultTxtName.style.color = 'red';
+    newResultTxtName.style.fontSize = '1.5em';
+    newResultTxtName.style.textAlign = 'center';
+    newResultTxtName.style.width = '100%';
     newResult.appendChild(newResultTxtName);
 }
+
+const showImgBox = (id) => {
+    ($nowPreviewed) ? closeImgBox() : '';
+    $previewMealsID = id.closest('li').getAttribute('id');
+    $resultsImgBoxes = document.querySelectorAll('.result-img-box');
+    $resultsImgBoxes[$previewMealsID].classList.remove('hidden-img-box');
+    $resultsImgBoxes[$previewMealsID].addEventListener('click', closeImgBox);
+    $nowPreviewed = $resultsImgBoxes[$previewMealsID];
+}
+
+const closeImgBox = () => {
+    $nowPreviewed.classList.add('hidden-img-box')
+}
+
+
 
 const selectMealfromList = (id) => {
     $choiceSection.classList.remove('nodisplay');
@@ -154,6 +202,7 @@ const selectMealfromList = (id) => {
     $resultsSection.classList.add('nodisplay');
     $backSection.classList.remove('nodisplay');
     $randomPropSection.classList.add('nodisplay');
+    ($nowPreviewed) ? closeImgBox() : '';
     displayChosenMeal();
     displayIngreadients();
     displayReceipe();
@@ -166,6 +215,7 @@ const listBack = () => {
 }
 
 const displayChosenMeal = () => {
+    $chosenMealImage.setAttribute('src', '');
     $chosenMealImage.setAttribute('src', $content[$selectedMealsID].strMealThumb);
     $chosenMealTitle.innerText = $content[$selectedMealsID].strMeal;
     $chosenMealDecription.innerText = $content[$selectedMealsID].strMeal;
